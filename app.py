c9a3731e6b3e02bb9648c3ef2598328cd4d56ba3
tony2265 <tony22657833@gmail.com>
2025-02-12 19:30:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/asr-sdk-python"))
 
 from flask import Flask, request, jsonify
 from ailabs_asr.streaming import StreamingClient
